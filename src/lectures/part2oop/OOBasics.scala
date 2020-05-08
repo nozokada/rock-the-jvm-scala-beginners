762647eef43760e05b6f1c03b5477d2ee973a747
Nozomi Okada <nokada@proofpoint.com>
2020-05-08 15:02:06 -0600

    Adding Counter

--- src/lectures/part2oop/OOBasics.scala
+++ src/lectures/part2oop/OOBasics.scala
@@ -6,6 +6,10 @@ object OOBasics extends App {
   val novel = new Novel("Clean Code", 2013, author)
   println(novel.authorAge)
   println(novel.isWrittenBy(anotherAuthor))
+
+  val counter = new Counter(0)
+  println(counter.increment())
+  println(counter.increment(5))
 }
 
 class Writer(firstName: String, surName: String, val year: Int) {
@@ -14,14 +18,15 @@ class Writer(firstName: String, surName: String, val year: Int) {
 
 class Novel(name: String, yearOfRelease: Int, author: Writer) {
   def authorAge: Int = yearOfRelease - author.year
-  def isWrittenBy(author: Writer): Boolean = this.author.fullName() == author.fullName() && this.author.fullName() == author.year
+  def isWrittenBy(author: Writer): Boolean = {
+    this.author.fullName() == author.fullName() && this.author.year == author.year
+  }
   def copy(newYearOfRelease: Int): Novel = new Novel(name, newYearOfRelease, author)
 }
 
-class Counter(value: Int) {
-  def currentCount(): Int = value
-  def increment(): Counter = new Counter(value + 1)
-  def decrement(): Counter = new Counter(value - 1)
-  def increment(amount: Int) = new Counter(value + amount)
-  def decrement(amount: Int) = new Counter(value - amount)
+class Counter(var count: Int) {
+  def increment(): Int = { count += 1; count}
+  def decrement(): Int = { count -= 1; count}
+  def increment(amount: Int): Int = { count += amount; count }
+  def decrement(amount: Int): Int = { count -= amount; count }
 }
