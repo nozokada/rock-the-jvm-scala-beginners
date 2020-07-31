a2d332ea2bd49b62f1e8563225b872c6ea1d859d
Nozomi Okada <nokada@proofpoint.com>
2020-07-31 15:04:13 -0600

    Adding Exceptions exercise

---
+++ src/exercises/part2oop/course22exceptions/Exceptions.scala
@@ -0,0 +1,47 @@
+package exercises.part2oop.course22exceptions
+
+object Exceptions extends App {
+//  Crash your program with an OutOfMemoryError
+//  val array = Array.ofDim(Int.MaxValue)
+
+//  Crash with SOError
+//  def infinite: Int = 1 + infinite
+//  val noLimit = infinite
+}
+
+class OverflowException extends RuntimeException
+class UnderflowException extends RuntimeException
+class MathCalculationException extends RuntimeException("Division by 0")
+
+object PocketCalculator {
+  def add(x: Int, y: Int): Int = {
+    val result = x + y
+
+    if(x > 0 && y > 0 && result < 0) throw new OverflowException
+    else if (x < 0 && y < 0 && result > 0) throw new UnderflowException
+    else result
+  }
+
+  def subtract(x: Int, y: Int): Int = {
+    val result = x - y
+
+    if(x > 0 && y > 0 && result < 0) throw new OverflowException
+    else if (x < 0 && y < 0 && result > 0) throw new UnderflowException
+    else result
+  }
+
+  def multiply(x: Int, y: Int): Int = {
+    val result = x * y
+
+    if(x > 0 && y > 0 && result < 0) throw new OverflowException
+    else if (x < 0 && y < 0 && result < 0) throw new OverflowException
+    else if (x > 0 && y < 0 && result > 0) throw new UnderflowException
+    else if (x < 0 && y > 0 && result > 0) throw new UnderflowException
+    else result
+  }
+
+  def divide(x: Int, y: Int): Int = {
+    if (y == 0) throw new MathCalculationException
+    else x / y
+  }
+}
