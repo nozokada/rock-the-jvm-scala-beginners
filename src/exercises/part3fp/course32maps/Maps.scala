348c32c8fe38aabfa72f8ee05e53773a9619b858
Nozomi Okada <nokada@proofpoint.com>
2020-08-25 12:44:43 -0600

    Adding Maps exercise

---
+++ src/exercises/part3fp/course32maps/Maps.scala
@@ -0,0 +1,30 @@
+package exercises.part3fp.course32maps
+
+object Maps extends App {
+  var network = Map("Nozomi" -> List("Bob"))
+
+  addPerson("Philipp")
+
+  println(network)
+
+  removePerson("Philipp")
+
+  println(network)
+
+  addFriend("Nozomi", "Dan")
+
+  println(network)
+
+  def addPerson(person: String): Unit = {
+    val newPairing = person -> List()
+    network += newPairing
+  }
+
+  def removePerson(person: String): Unit = {
+    network -= person
+  }
+
+  def addFriend(person: String, friend: String): Unit = {
+    network ++= network.filterKeys(key => key == person).mapValues(value => value ++ List(friend))
+  }
+}
