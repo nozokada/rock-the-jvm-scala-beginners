8259a950cfa3e1814a716795f3aee45c797e4575
Nozomi Okada <nokada@proofpoint.com>
2020-09-15 11:54:37 -0600

    Using Set instead of List and adding friends mutually

--- src/exercises/chapter4FunctionalProgramming/lecture32maps/Maps.scala
+++ src/exercises/chapter4FunctionalProgramming/lecture32maps/Maps.scala
@@ -1,9 +1,10 @@
 package exercises.chapter4FunctionalProgramming.lecture32maps
 
 object Maps extends App {
-  var network = Map("Nozomi" -> List("Bob"))
+  var network = Map("Nozomi" -> Set("Bob"))
 
   addPerson("Philipp")
+  addPerson("Dan")
 
   println(network)
 
@@ -12,11 +13,12 @@ object Maps extends App {
   println(network)
 
   addFriend("Nozomi", "Dan")
+  addFriend("Nozomi", "Dan")
 
   println(network)
 
   def addPerson(person: String): Unit = {
-    val newPairing = person -> List()
+    val newPairing = person -> Set[String]()
     network += newPairing
   }
 
@@ -25,6 +27,11 @@ object Maps extends App {
   }
 
   def addFriend(person: String, friend: String): Unit = {
-    network ++= network.filterKeys(key => key == person).mapValues(value => value ++ List(friend))
+    network ++= network.filterKeys(key => key == person).mapValues(value => value ++ Set(friend))
+    network ++= network.filterKeys(key => key == friend).mapValues(value => value ++ Set(person))
+  }
+
+  def unfriend(person: String, friend: String): Unit = {
+
   }
 }
