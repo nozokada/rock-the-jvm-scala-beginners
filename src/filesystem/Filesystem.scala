921d2b2ee942dc4b53fcbcd4fb7cec386d4a03eb
Nozomi Okada <nokada@proofpoint.com>
2020-11-20 09:44:27 -0700

    Added filesystem package

---
+++ src/filesystem/Filesystem.scala
@@ -0,0 +1,19 @@
+package filesystem
+
+import java.util.Scanner
+
+import filesystem.commands.Command
+import filesystem.files.Directory
+
+object Filesystem extends App {
+
+  val root = Directory.ROOT
+  var state = State(root, root)
+  val scanner = new Scanner(System.in)
+
+  while(true) {
+    state.show()
+    val input = scanner.nextLine()
+    state = Command.from(input).apply(state)
+  }
+}
---
+++ src/filesystem/commands/Mkdir.scala
@@ -0,0 +1,27 @@
+package filesystem.commands
+import filesystem.State
+import filesystem.files.Directory
+
+class Mkdir(name: String) extends Command {
+
+  override def apply(state: State): State = {
+    val wd = state.wd
+    if (wd.hasEntry(name)) {
+      state.setMessage("Entry " + name + " already exists!")
+    } else if (name.contains(Directory.SEPARATOR)) {
+      state.setMessage(name + " must not contain separators!")
+    } else if (checkIllegal(name)) {
+      state.setMessage(name + ": illegal entry name!")
+    } else {
+      doMkdir(state, name)
+    }
+  }
+
+  def checkIllegal(str: String): Boolean = {
+    name.contains(".")
+  }
+
+  def doMkdir(state: State, str: String): State = {
+    ???
+  }
+}
